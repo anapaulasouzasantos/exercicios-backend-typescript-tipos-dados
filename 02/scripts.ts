665fc8367d42f8d7eb849e91db2fc2583720a75f
Ana Paula Souza dos Santos <anapaulasouza2095@gmail.com>
2023-06-05 15:50:06 -0300

    finalizado

---
+++ 02/scripts.ts
@@ -0,0 +1,48 @@
+import { Console } from "console";
+
+const lista = [
+    {
+        nome: "Guido",
+        idade: 32,
+        status: true,
+    },
+    {
+        nome: "Dani",
+        idade: 30,
+        status: true,
+    },
+    {
+        nome: "João",
+        idade: 40,
+        status: false,
+    },
+    {
+        nome: "Guilherme",
+        idade: 29,
+        status: true,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+        status: false,
+    },
+    {
+        nome: "José",
+        idade: 28,
+        status: false,
+    },
+]
+
+const filtrarUsuarios = (
+    usuarios:
+        { nome: string, idade: number, status: boolean }[],
+    usuario: string
+): { nome: string, idade: number, status: boolean }[] => {
+    const resultado = usuarios.filter((usuarioLista) => {
+        return usuarioLista.nome.toLowerCase().includes(usuario.toLowerCase())
+    });
+
+    return resultado;
+}
+
+console.log(filtrarUsuarios(lista, 'Jo'));
